11dce14fd78ecd2598898d0c9997339b18ecd1cc
Mickael Mangaud <mickaelmangaud@gmail.com>
2020-12-31 17:31:11 +0100

    folders structure + init express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
