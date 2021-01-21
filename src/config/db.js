804be7434e7c1e299959963062167300c3b81eb9
Mickael Mangaud <mickaelmangaud@gmail.com>
2021-01-21 13:02:26 +0100

    comments

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,3 @@
-// import mongoose from 'mongoose';
-
-// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// mongoose.set('useCreateIndex', true);
-
-// export const db = mongoose.connection;
-
-// db.on('error', (error) => console.log('mongoose Error', error));
-
-// db.on('open', () => {
-//   console.log('MONGOOSE CONNECTION OK');
-// });
 import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize(
